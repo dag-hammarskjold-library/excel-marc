ec1d7a16d705e572690f3cfc1ccd67f0b580f02c
jbukhari <joe.bukhari@gmail.com>
2020-01-21 17:18:27 -0500

    first commit

---
+++ run.ps1
@@ -0,0 +1,19 @@
+.\env\Scripts\activate
+Add-Type -AssemblyName System.Windows.Forms
+
+$chooser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ InitialDirectory = [Environment]::GetFolderPath('Desktop') }
+$chooser.ShowDialog()
+$fn = $chooser.filename
+$out = "$($fn).mrc"
+
+$exit = excel_marc --connect=$env:mdb --file=$fn --type=bib --format=mrc > $out
+
+if (!$?) {
+	powershell -noexit
+}
+
+write-output $out | set-clipboard 
+write-output "MARC file: " $out `n` "The file path has been added to your clipboard" `n`
+
+powershell -noexit
+
